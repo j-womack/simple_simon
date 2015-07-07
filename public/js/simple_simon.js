d3f66f72f14a135049cd863fe30d818ada541fb6
j-womack <womack.joshua@gmail.com>
2015-07-07 11:59:28 -0500

    Day 2 noontime progress

--- public/js/simple_simon.js
+++ public/js/simple_simon.js
@@ -1,55 +1,101 @@
 "use strict";
+
+// declaring the buttons array and empty sequences
 var buttons = [btn1, btn2, btn3, btn4];
 var simonSeq = [];
 var yourSeq = [];
 
+// animation function
 function animateSquare(square){
     $(square).animate({
         opacity: 0.25,
         top: "+=5",
         left: "+=5"
-    }, 100);
-
+    }, 200);
     $(square).animate({
         opacity: 1,
         top: "-=5",
         left: "-=5",
         "box-shadow": 0
-    }, 100);
-    // $(buttons[sequence[i]]).delay(200);
+    }, 200);
 };
 
+// running the randomized sequence
+function playSimonSeq() {
+    var i=0;
+    var intervalId = setInterval(function() {
+        if(i>=simonSeq.length){
+            clearInterval(intervalId);
+        }
+        animateSquare(buttons[simonSeq[i]]);
+        i++
+    }, 500); 
+    yourSeq = []; 
+    $('.round').html('Round: ' + simonSeq.length);
+};
 
-$('#start').click(function(){
+// selecting random numbers and pushing to simon seq
+function getRandom() {
     var decision = Math.floor((Math.random() * 4));
 
     simonSeq.push(decision);
     console.log("Simon: [" + simonSeq + "]");
-        
-    for (var i=0; i<simonSeq.length; i++){
-        setTimeout(animateSquare(buttons[simonSeq[i]]),200);
+};
 
-    };
-});
+// disables start button
+function disableStart() {
+    $('#start').off('click');
+};
 
-$('.button').click(function(e) {
-    var squareClicked = $(this);
-    var buttonPosition
-    switch (squareClicked.context.id) {
-        case "btn1":
-            buttonPosition = 0;
-            break;
-        case "btn2":
-            buttonPosition = 1;
-            break;
-        case "btn3":
-            buttonPosition = 2;
-            break;
-        case "btn4":
-            buttonPosition = 3;
-            break;
-    }
-    yourSeq.push(buttonPosition);
-    animateSquare(squareClicked);
-    console.log("You: [" + yourSeq + "]");
-});
+// listens for the start button and runs the Simon function
+function enableStart() {
+    $('#start').click(function(){
+
+        //      MOCKUP
+        // off();
+        // setTimeout(enable, simonSeq.length
+
+
+        //      NOTES
+        // $(this).off('click');
+        // setTimeout(function(){
+        //     $(this).on('click', function(){
+        //     });
+        // }, (simonSeq.length * 1000 + 1));
+
+        getRandom();
+        playSimonSeq();
+    });  
+};
+
+function disableButtons() {
+    $('.button').off('click');
+};
+
+function enableButtons() {
+    // listens for user clicks on the buttons
+    $('.button').click(function(e) {
+        var squareClicked = $(this);
+        var buttonPosition
+        switch (squareClicked.context.id) {
+            case "btn1":
+                buttonPosition = 0;
+                break;
+            case "btn2":
+                buttonPosition = 1;
+                break;
+            case "btn3":
+                buttonPosition = 2;
+                break;
+            case "btn4":
+                buttonPosition = 3;
+                break;
+        }
+        yourSeq.push(buttonPosition);
+        animateSquare(squareClicked);
+        console.log("You: [" + yourSeq + "]");
+    });
+};
+
+enableStart();
+enableButtons();
